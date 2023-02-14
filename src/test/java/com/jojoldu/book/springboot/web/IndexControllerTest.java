9e5c1163e4a0baff73ac48edec8c3b956c5861fa
Moi1oM <hcloud0806@gmail.com>
2023-02-14 18:17:43 +0900

    2023.02.14 챕터5 Test Code

--- src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -20,7 +20,7 @@ public class IndexControllerTest {
     public void 메인페이지_로딩() {
         String body = this.restTemplate.getForObject("/", String.class);
 
-        assertThat(body).contains("스프링 부트로 시작하는 웹서비스");
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스 Ver.2");
     }
 
 }
